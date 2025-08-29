7fd231717a7eff7216bab5b086efae7855d3020d
Jay Sorg <jay.sorg@gmail.com>
2025-08-29 02:18:29 -0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,85 +1,56 @@
 #!/bin/sh
 
-if test -d "../rdpc"
-then
-    echo "Directory ../rdpc exists"
-else
-    echo "Directory ../rdpc does not exits"
-    exit 1
-fi
+listOfProjects="librfxcodec librlecodec rdpc svc cliprdr rdpsnd xclient"
+
+# check that all projects exist
+for proj in $listOfProjects
+do
+    if test -d "../$proj"
+    then
+        :
+    else
+        echo "Directory ../$proj does not exits"
+        exit 1
+    fi
+done
+echo "all projects exist"
+
+# clean
 if [ "$1" = "clean" ]
 then
-    cd ../rdpc
-    rm -f -r .zig-cache/ zig-out/
-    cd ../svc
-    rm -f -r .zig-cache/ zig-out/
-    cd ../cliprdr
-    rm -f -r .zig-cache/ zig-out/
-    cd ../rdpsnd
-    rm -f -r .zig-cache/ zig-out/
-    cd ../xclient
-    rm -f -r .zig-cache/ zig-out/
+    for proj in $listOfProjects
+    do
+        echo "clean ../$proj"
+        cd ../$proj
+        rm -f -r .zig-cache/ zig-out/
+    done
     exit 0
 fi
 
+# debug build
 if [ "$1" = "debug" ]
 then
-    cd ../rdpc
-    zig build --summary all
-    if test $? -ne 0
-    then
-        exit 1
-    fi
-    cd ../svc
-    zig build --summary all
-    if test $? -ne 0
-    then
-        exit 1
-    fi
-    cd ../cliprdr
-    zig build --summary all
-    if test $? -ne 0
-    then
-        exit 1
-    fi
-    cd ../rdpsnd
-    zig build --summary all
-    if test $? -ne 0
-    then
-        exit 1
-    fi
-    cd ../xclient
-    zig build --summary all
-    if test $? -ne 0
-    then
-        exit 1
-    fi
+    for proj in $listOfProjects
+    do
+        echo "building debug ../$proj"
+        cd ../$proj
+        zig build --summary all
+        if test $? -ne 0
+        then
+            exit 1
+        fi
+    done
     exit 0
 fi
 
-cd ../rdpc
-zig build -Doptimize=ReleaseFast -Dstrip=true --summary all
-if test $? -ne 0
-then
-    exit 1
-fi
-cd ../svc
-zig build -Doptimize=ReleaseFast -Dstrip=true --summary all
-if test $? -ne 0
-then
-    exit 1
-fi
-cd ../cliprdr
-zig build -Doptimize=ReleaseFast -Dstrip=true --summary all
-if test $? -ne 0
-then
-    exit 1
-fi
-cd ../rdpsnd
-zig build -Doptimize=ReleaseFast -Dstrip=true --summary all
-if test $? -ne 0
-then
-    exit 1
-fi
-cd ../xclient
-zig build -Doptimize=ReleaseFast -Dstrip=true --summary all
+# release build
+for proj in $listOfProjects
+do
+    echo "building ../$proj"
+    cd ../$proj
+    zig build -Doptimize=ReleaseFast -Dstrip=true --summary all
+    if test $? -ne 0
+    then
+        exit 1
+    fi
+done
